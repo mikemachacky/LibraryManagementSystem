7e3a56ea5351092e136bf643cdae45d08074094b
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-05-06 09:52:36 +0200

    Dependencies not working properly

--- Library/Library/Library/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Library.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
