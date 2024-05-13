33fe384c2944b9d12c7ad6ff574d8c63bc7742b0
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-05-13 09:55:16 +0200

    Deleted

--- BlazorLibrary/BlazorLibrary/Data/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorLibrary.Data
-{
-    public class Role
-    {
-        public string Id { get; set; } = string.Empty;
-        public string RoleName { get; set; } = string.Empty;
-    }
-}
