60d956cda29e20290a1e96ff6761a8ad63ff6e28
magda_m <79257135+mikemachacky@users.noreply.github.com>
2024-05-15 15:18:19 +0200

    IBookService CRUD updated, remove added

--- BlazorServer/Services/BookService.cs
+++ BlazorServer/Services/BookService.cs
@@ -12,9 +12,24 @@ namespace BlazorServer.Services
         {
             _appDbContext = appDbContext;
         }
+
+        public async Task<Book> AddBook(Book book)
+        {
+            _appDbContext.Books.Add(book);
+            await _appDbContext.SaveChangesAsync();
+            return book;
+        }
+
         public async Task<List<Book>> GetAllBooks()
         {
            return await _appDbContext.Books.ToListAsync();
+        }
+
+        public async Task<Book> RemoveBook(Book book)
+        {
+            _appDbContext.Books.Remove(book);
+            await _appDbContext.SaveChangesAsync();
+            return book;
         }
     }
 }
--- BlazorServer/Services/IBookService.cs
+++ BlazorServer/Services/IBookService.cs
@@ -5,5 +5,7 @@ namespace BlazorServer.Services
     public interface IBookService
     {
         Task<List<Book>> GetAllBooks();
+        Task<Book> AddBook(Book book);
+        Task<Book> RemoveBook(Book book);
     }
 }
